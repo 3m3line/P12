70425125593357c9a2e5a90ea8244b1a6c8170e6
3m3line <emeline.boureaud@gmail.com>
2024-10-09 19:44:06 +0200

    Debut code page home

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,69 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import NavBar from './Containers/NavBar'
+import Button from './Components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <header>
+      <NavBar></NavBar>
+    </header>
+    <main>
+      <h1 className='visually-hidden'></h1>
+      <section className='filters'>
+        <h2 className='visually-hidden'>Choisissez un tag pour filtrer les technologies</h2>
+        <div>
+          <Button 
+            text="Html" 
+            className="btn-filter" 
+            onClick={() => {}}
+          />
+          <Button 
+            text="Javascript" 
+            className="btn-filter" 
+            onClick={() => {}}
+          />
+          <Button 
+            text="React" 
+            className="btn-filter" 
+            onClick={() => {}}
+          />
+          <Button 
+            text="Wordpress" 
+            className="btn-filter" 
+            onClick={() => {}}
+          />
+          <Button 
+            text="CSS" 
+            className="btn-filter" 
+            onClick={() => {}}
+          />
+        </div>
+      </section>
+      <section className='contenu-page'>
+        <article className='section-a-propos'>
+          <h2 className='fancy-text'>A propos</h2>
+          <p></p>
+        </article>
+        <div className='image-presentation'>
+          <h2 className='fancy-text'>Skills</h2>
+          <div>
+            <img src="./src/assets/fleche-gauche.png" alt="fleche" className='fleche-gauche'/>
+            <img src="./src/assets/fleche-haut.png" alt="fleche" className='fleche-haut'/>
+            <img src="./src/assets/fleche-droite.png" alt="fleche" className='fleche-droite'/>
+          </div>
+          <img src="./src/assets/test.png" alt="Représentation en dessin de votre développeuse Web Front" className='dessin'/>
+        </div>
+        <article className='section-contact'>
+          <h2 className='fancy-text'>Me contacter</h2>
+        </article>
+
+      </section>
+    </main>
+      
     </>
   )
 }
---
+++ src/Containers/NavBar.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './NavBar.scss';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div>Emeline <br></br>Boureaud</div>
+            <ul><li>Site statique</li>
+            <li>Site dynamique</li>
+            <li>Contact</li></ul>
+        </nav>
+)}
+
+export default NavBar;
