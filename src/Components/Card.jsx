f3c5238457d21d98d452e4136a9526b16bd48c58
3m3line <emeline.boureaud@gmail.com>
2024-10-17 13:18:23 +0200

    barre overflox

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -56,23 +56,25 @@ const HoverCard = ({ project }) => {
                   &times;
                   </span>
                 </div>
-                {project.lienSite ? (
-                  <a href={project.lienSite} target="_blank" rel="noopener noreferrer">
-                      <img src={project.cover} alt="image projet" className='image-lien'/>
-                  </a>
-                ) : (
-                  <img src={project.cover} alt="image projet" />
-                )}
-                <div className='tag-container'>
-                  {project.technologie.map((tech, index) => (
-                    <Tag key={index} text={tech} />
-                  ))}
-                </div>
-                <h2>{project.title}</h2>
-                <p>{project.description}</p>
-                <div className='liens-modale'>
-                  <div className='lienSite'><a href={project.lienSite} target="_blank" rel="noopener noreferrer" className={project.lienSite === '' ? 'visually-hidden' : ''}>Lien vers le site</a></div>
-                  <div className='lienCode'><a href={project.lienCode} target="_blank" rel="noopener noreferrer" className={project.lienCode === '' ? 'visually-hidden' : ''}>Lien vers le code</a></div>
+                <div className='modal-overflow'>
+                  {project.lienSite ? (
+                    <a href={project.lienSite} target="_blank" rel="noopener noreferrer">
+                        <img src={project.cover} alt="image projet" className='image-lien'/>
+                    </a>
+                  ) : (
+                    <img src={project.cover} alt="image projet" />
+                  )}
+                  <div className='tag-container'>
+                    {project.technologie.map((tech, index) => (
+                      <Tag key={index} text={tech} />
+                    ))}
+                  </div>
+                  <h2>{project.title}</h2>
+                  <p>{project.description}</p>
+                  <div className='liens-modale'>
+                    <div className='lienSite'><a href={project.lienSite} target="_blank" rel="noopener noreferrer" className={project.lienSite === '' ? 'visually-hidden' : ''}>Lien vers le site</a></div>
+                    <div className='lienCode'><a href={project.lienCode} target="_blank" rel="noopener noreferrer" className={project.lienCode === '' ? 'visually-hidden' : ''}>Lien vers le code</a></div>
+                  </div>
                 </div>
             </div>
             </div>
